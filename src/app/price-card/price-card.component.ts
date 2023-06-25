808653bdac47e9f0c05958f840f433cbd6759b2a
Gautier <gautier.edel.info@gmail.com>
2023-06-25 18:26:22 +0200

    dynamic price card generation

--- src/app/price-card/price-card.component.ts
+++ src/app/price-card/price-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { PriceCardInterface } from '../interfaces/PriceCardInterface';
 
 @Component({
   selector: 'app-price-card',
   templateUrl: './price-card.component.html',
-  styleUrls: ['./price-card.component.css']
+  styleUrls: ['./price-card.component.css'],
 })
 export class PriceCardComponent {
-
+  @Input() cardData!: PriceCardInterface;
 }
